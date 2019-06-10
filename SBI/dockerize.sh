748b9e77ce9ed6bef3d4021cafdae9eef86bd019
Subham Mandal <subhammandal01@gmail.com>
2019-06-10 15:05:19 +0530

    Update dockerize.sh

--- SBI/dockerize.sh
+++ SBI/dockerize.sh
@@ -4,30 +4,30 @@ export PATH="$PATH:/usr/local/bin"
 case $BRANCH_NAME in
   qa)
    if [ -n "$(echo $BRANCH_NAME|grep '[a-zA-Z]')" ];then
-    tag="$BUILD_ID-$(cat pom.xml |grep '<version>'|head -1|sed 's|<version>||; s|</version>||')"
+    tag=$BUILD_ID-$(cat pom.xml |grep '<version>'|head -1|sed 's|<version>||; s|</version>||')
     dockerfile=Dockerfile.develop
     fi
     ;;
   develop)
   if [ -n "$(echo $BRANCH_NAME|grep '[a-zA-Z]')" ];then
-    tag="$BUILD_ID-$(cat pom.xml |grep '<version>'|head -1|sed 's|<version>||; s|</version>||')"
+    tag=$BUILD_ID-$(cat pom.xml |grep '<version>'|head -1|sed 's|<version>||; s|</version>||')
     dockerfile=Dockerfile.develop
     fi
     ;;
   master)
   if [ -n "$(echo $BRANCH_NAME|grep '[a-zA-Z]')" ];then
-   tag="$BUILD_ID-$(cat pom.xml |grep '<version>'|head -1|sed 's|<version>||; s|</version>||')"
+   tag=$BUILD_ID-$(cat pom.xml |grep '<version>'|head -1|sed 's|<version>||; s|</version>||')
    dockerfile=Dockerfile.prod
    fi
     ;;
   *)
   echo $BRANCH_NAME
-    tag="$BUILD_ID-$(cat pom.xml |grep '<version>'|head -1|sed 's|<version>||; s|</version>||')"
+    tag=$BUILD_ID-$(cat pom.xml |grep '<version>'|head -1|sed 's|<version>||; s|</version>||')
     dockerfile=Dockerfile.develop
 esac
 
 services=$(cat $(dirname $0)/service-manifest.txt)
 for s in $services
 do
-docker build -t ${s}:$tag  -f $(dirname $0)/docker/$dockerfile .
+docker build -t $s:$tag  -f $(dirname $0)/docker/$dockerfile .
 done
