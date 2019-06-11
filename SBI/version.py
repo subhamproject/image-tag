1ed22161b85d905d547dc72d22bf287775c14847
Subham Mandal <subhammandal01@gmail.com>
2019-06-11 16:52:57 +0530

    Update version.py

--- SBI/version.py
+++ SBI/version.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 import xml.etree.ElementTree as ElementTree
-doc = ElementTree.parse('../../pom.xml')
+doc = ElementTree.parse('pom.xml')
 print(doc.find('{http://maven.apache.org/POM/4.0.0}version').text)
