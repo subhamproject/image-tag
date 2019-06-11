9f227940bb603d8c488aa102814f6b2b77a28ae5
Subham Mandal <subhammandal01@gmail.com>
2019-06-11 11:47:21 +0530

    Update function.sh

--- SBI/lib/function.sh
+++ SBI/lib/function.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+DIR=$(dirname $0)
 function version() {
-docker-compose -f ../test-bed.yml run --rm -w "$WORKSPACE" --name maven-${BUILD_NUMBER} --entrypoint "SBI/version.sh" maven-app-build
+docker-compose -f $DIR/test-bed.yml run --rm -w "$WORKSPACE" --name maven-${BUILD_NUMBER} --entrypoint "SBI/version.sh" maven-app-build
 }
 
 
